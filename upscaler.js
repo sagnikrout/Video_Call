f93ab59e606cd59246ca690e0e576606c7ee430b
Developer <developer@example.com>
2026-08-10 01:35:54 +0530

    fix: invert WebGL texture Y-axis to correct upside-down video

--- upscaler.js
+++ upscaler.js
@@ -125,6 +125,7 @@ function initUpscaler(videoElement, canvasElement) {
     // Create Texture
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
